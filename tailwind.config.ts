cb92e33eaa89dbefe82524c6c386894c51115b20
latifnjimoluh <159473076+latifnjimoluh@users.noreply.github.com>
2025-08-08 04:50:40 +0100

    feat: update theme styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,10 +80,20 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "spin-slow": {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        "spin-slower": {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(-360deg)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "spin-slow": "spin-slow 3s linear infinite",
+        "spin-slower": "spin-slower 6s linear infinite",
       },
       fontFamily: {
         sans: ["var(--font-inter)"],
